130b2a1a0234f4dd96355dba4e47d77f0d6ee758
HrishiB9 <hrishibelgamwar@gmail.com>
2024-09-12 08:18:40 +0530

    Added Testing class

---
+++ JavaPrograms/src/BasicJavaProgram/TestClass.java
@@ -0,0 +1,13 @@
+package BasicJavaProgram;
+
+public class TestClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+
+	}
+
+}
